3fcdfa6b6eeb91d344d6d0671b6dbdebb90ff46a
raj1905 <rajendiran@aymcommerce.com>
2021-03-22 10:50:48 -0700

    refactor

--- app.module.js
+++ app.module.js
@@ -5,152 +5,12 @@ import {
   Toast,
   CrudManager,
   SchedulerEventModel,
-} from "../../build/schedulerpro.module.js";
-import shared from "../_shared/shared.module.js";
+} from "../../build/schedulerpro.module.js"
+import shared from "../_shared/shared.module.js"
 
-const cookie = "PHPSESSID=schedulerpro-crudmanager";
+const cookie = "PHPSESSID=schedulerpro-crudmanager"
 if (!document.cookie.includes(cookie)) {
-  document.cookie = `${cookie}-${Math.random().toString(16).substring(2)}`;
-}
-
-class Drag extends DragHelper {
-  static get defaultConfig() {
-    return {
-      // Don't drag the actual row element, clone it
-      cloneTarget: true,
-      mode: "translateXY",
-      // Only allow drops on the schedule area
-      dropTargetSelector: ".b-timeline-subgrid",
-      // Only allow drag of row elements inside on the unplanned grid
-      targetSelector: ".b-grid-row:not(.b-group-row)",
-    };
-  }
-
-  construct(config) {
-    const me = this;
-
-    super.construct(config);
-
-    me.on({
-      dragstart: me.onTaskDragStart,
-      drag: me.onTaskDrag,
-      drop: me.onTaskDrop,
-      thisObj: me,
-    });
-  }
-
-  onTaskDragStart({ context }) {
-    const me = this,
-      { schedule } = me,
-      mouseX = context.clientX,
-      proxy = context.element,
-      task = me.grid.getRecordFromElement(context.grabbed),
-      newSize = me.schedule.timeAxisViewModel.getDistanceForDuration(
-        task.durationMS
-      );
-
-    // save a reference to the task so we can access it later
-    context.task = task;
-
-    // Mutate dragged element (grid row) into an event bar
-    proxy.classList.remove("b-grid-row");
-    proxy.classList.add("b-sch-event-wrap");
-    proxy.classList.add("b-unassigned-class");
-    proxy.classList.add(`b-${schedule.mode}`);
-    proxy.innerHTML = `<i class="${task.iconCls}"></i> ${task.name}`;
-
-    me.schedule.enableScrollingCloseToEdges(me.schedule.timeAxisSubGrid);
-
-    if (schedule.isHorizontal) {
-      // If the new width is narrower than the grabbed element...
-      if (context.grabbed.offsetWidth > newSize) {
-        const proxyRect = Rectangle.from(context.grabbed);
-
-        // If the mouse is off (nearly or) the end, centre the element on the mouse
-        if (mouseX > proxyRect.x + newSize - 20) {
-          context.newX = context.elementStartX = context.elementX =
-            mouseX - newSize / 2;
-          DomHelper.setTranslateX(proxy, context.newX);
-        }
-      }
-
-      proxy.style.width = `${newSize}px`;
-    } else {
-      const width = schedule.resourceColumns.columnWidth;
-
-      // Always center horizontal under mouse for vertical mode
-      context.newX = context.elementStartX = context.elementX =
-        mouseX - width / 2;
-      DomHelper.setTranslateX(proxy, context.newX);
-
-      proxy.style.width = `${width}px`;
-      proxy.style.height = `${newSize}px`;
-    }
-
-    // Prevent tooltips from showing while dragging
-    schedule.element.classList.add("b-dragging-event");
-  }
-
-  onTaskDrag({ event, context }) {
-    const me = this,
-      coordinate = DomHelper[
-        `getTranslate${me.schedule.isHorizontal ? "X" : "Y"}`
-      ](context.element),
-      date = me.schedule.getDateFromCoordinate(coordinate, "round", false),
-      // Coordinates required when used in vertical mode, since it does not use actual columns
-      resource =
-        context.target &&
-        me.schedule.resolveResourceRecord(context.target, [
-          event.offsetX,
-          event.offsetY,
-        ]);
-
-    // Don't allow drops anywhere, only allow drops if the drop is on the timeaxis and on top of a Resource
-    context.valid = context.valid && Boolean(date && resource);
-
-    // Save reference to resource so we can use it in onTaskDrop
-    context.resource = resource;
-  }
-
-  // Drop callback after a mouse up, take action and transfer the unplanned task to the real SchedulerEventStore (if it's valid)
-  onTaskDrop({ context, event }) {
-    const me = this,
-      { task, target } = context;
-
-    me.schedule.disableScrollingCloseToEdges(me.schedule.timeAxisSubGrid);
-
-    // If drop was done in a valid location, set the startDate and transfer the task to the Scheduler event store
-    if (context.valid && target) {
-      const coordinate = DomHelper[
-          `getTranslate${me.schedule.isHorizontal ? "X" : "Y"}`
-        ](context.element),
-        date = me.schedule.getDateFromCoordinate(coordinate, "round", false),
-        // Try resolving event record from target element, to determine if drop was on another event
-        targetEventRecord = me.schedule.resolveEventRecord(context.target);
-
-      if (date) {
-        // Remove from grid first so that the data change
-        // below does not fire events into the grid.
-        me.grid.store.remove(task);
-
-        //task.setStartDate(date, true);
-        task.startDate = date;
-        task.resource = context.resource;
-        me.schedule.eventStore.add(task);
-      }
-
-      // Dropped on a scheduled event, display toast
-      if (targetEventRecord) {
-        WidgetHelper.toast(`Dropped on ${targetEventRecord.name}`);
-      }
-
-      me.context.finalize();
-    } else {
-      me.abort();
-    }
-
-    me.schedule.element.classList.remove("b-dragging-event");
-  }
+  document.cookie = `${cookie}-${Math.random().toString(16).substring(2)}`
 }
 
 const customCrudManager = new CrudManager({
@@ -192,7 +52,7 @@ const customCrudManager = new CrudManager({
 
     console.error(exceptionText);
   },
-});
+})
 
 const scheduler = new Scheduler({
   appendTo: "container",
@@ -252,7 +112,9 @@ const scheduler = new Scheduler({
       onAction() {
         scheduler.crudManager
           .load({
-            // Adding a query string parameter "...&reset=1" to let server know that we want to reset the database
+            // Adding a query string parameter 
+            // "...&reset=1" 
+            // to let server know that we want to reset the database
             request: {
               params: {
                 reset: 1,
@@ -260,13 +122,13 @@ const scheduler = new Scheduler({
             },
           })
           .then(() => WidgetHelper.toast("Database was reset"))
-          .catch(() => WidgetHelper.toast("Database reset failed"));
+          .catch(() => WidgetHelper.toast("Database reset failed"))
       },
     },
   ],
-});
+})
 
-const e = React.createElement;
+const e = React.createElement
 
 const unplannedItems = [
   {
@@ -346,43 +208,20 @@ const unplannedItems = [
     durationUnit: "h",
     iconCls: "b-fa b-fa-fw b-fa-cog",
   },
-];
+]
 
 class SideMenu extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = { unplannedItems };
+    this.state = { unplannedItems }
 
-    this.onDrag = this.onDrag.bind(this);
-    console.log("onInit", scheduler.eventStore);
-
-    // const jsonArray = scheduler.eventStore.toJSON();
-    // scheduler.crudManager.load({
-    //   // Adding a query string parameter "...&reset=1" to let server know that we want to reset the database
-    //   request: {
-    //     params: {
-    //       data: jsonArray,
-    //     },
-    //   },
-    // });
-    // scheduler.eventStore.add(
-    //   new SchedulerEventModel({
-    //     startDate: "2018-05-21T08:00:00",
-    //     durationUnit: "hour",
-    //     duration: 2,
-    //     name: "anbu",
-    //     id: 10,
-    //   })
-    // );
-
-    // console.log("onInit2", scheduler.eventStore);
-  }
+    this.onDrag = this.onDrag.bind(this)
+ }
 
   onDrag(index, event, data) {
-    console.log({ event });
-    const jsonArray = scheduler.eventStore.toJSON();
-    console.log("data", data);
+    const jsonArray = scheduler.eventStore.toJSON()
+
     let droppedTask = new SchedulerEventModel({
       startDate: "2018-05-01T08:00:00",
       durationUnit: "hour",
@@ -390,12 +229,14 @@ class SideMenu extends React.Component {
       name: data.name,
       id: jsonArray.length + 1,
       resourceId: 1,
-    });
+    })
 
     if (!jsonArray.some((person) => person.name === data.name)) {
-      scheduler.crudManager.sync();
+      scheduler.crudManager.sync()
       scheduler.crudManager.load({
-        // Adding a query string parameter "...&reset=1" to let server know that we want to reset the database
+        // Adding a query string parameter 
+        // "...&reset=1" 
+        // to let server know that we want to reset the database
         request: {
           params: {
             data: jsonArray,
@@ -404,11 +245,8 @@ class SideMenu extends React.Component {
       });
       scheduler.eventStore.add(droppedTask);
     }
-    const unplannedItems = this.state.unplannedItems.filter(
-      (_, i) => i != index
-    );
-    this.setState({ unplannedItems });
-    console.log("drag", scheduler.eventStore);
+    
+    this.setState({ unplannedItems: this.state.unplannedItems.filter((_, idx) => idx != index) })
   }
 
   render() {
@@ -458,9 +296,9 @@ class SideMenu extends React.Component {
           )
         )
       )
-    );
+    )
   }
 }
 
-const domContainer = document.querySelector("#sideMenu");
-ReactDOM.render(e(SideMenu), domContainer);
+const domContainer = document.querySelector("#sideMenu")
+ReactDOM.render(e(SideMenu), domContainer)
